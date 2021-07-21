ab07e9057adb904eb905ed1f7f38bf7719a81f2b
Jonathan Balkind <jbalkind@ucsb.edu>
2021-07-21 15:15:04 -0700

    style refactoring

--- ahb_lite/ahb_lite_tb.v
+++ ahb_lite/ahb_lite_tb.v
@@ -4,23 +4,46 @@ module Test_AHB_Lite ();
 
     /* Make a regular pulsing clock. */
     reg clock = 1;
-    always #1 clock = !clock;
+    reg rst_n = 1'b0;
+    always #1 clock = ~clock;
 
     wire write, trans, readyout;
     wire[7:0] waddr, wdata, rdata, value;
 
-    Peripheral perry (clock, write, trans, waddr, wdata, readyout, rdata);
-    Controller #(.VALUE(7)) conroy (clock, readyout, rdata, write, trans, waddr, wdata, value);
+    Peripheral perry (
+        .clock      (clock)
+    ,   .rst_n      (rst_n)
+    ,   .write      (write)
+    ,   .trans      (trans)
+    ,   .waddr      (waddr)
+    ,   .wdata      (wdata)
+    ,   .readyout   (readyout)
+    ,   .rdata      (rdata)
+    );
+    Controller #(.VALUE(7)) conroy (
+        .clock      (clock)
+    ,   .rst_n      (rst_n)
+    ,   .readyout   (readyout)
+    ,   .rdata      (rdata)
+    ,   .write      (write)
+    ,   .trans      (trans)
+    ,   .waddr      (waddr)
+    ,   .wdata      (wdata)
+    ,   .value      (value)
+    );
 
     initial begin
         $dumpfile("ahb_lite.vcd"); 
         $dumpvars(0, Test_AHB_Lite);
 
+        #10;
+        rst_n = 1'b1;
+
         #256 $finish;
     end
 
     always @(posedge clock) begin
-        if (value == 1) $finish;
+        if (value === 8'b1) $finish;
     end
 
 endmodule
